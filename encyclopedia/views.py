41e70d3aa1c947805d35b45ac293dd95b94faae0
Ziga Pregelj - Asus <ziga.pregelj@razum.si>
2023-02-17 21:42:22 +0100

    First task --> Entry Page done

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+import markdown2
 from . import util
 
 
@@ -8,3 +8,15 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def entries(request, title):
+    if title.capitalize() or title.uppercase() or title.lowercase in util.list_entries():
+        return render(request, "encyclopedia/entry.html", {
+            "entry": markdown2.markdown(util.get_entry(title)),
+            "page_title": title.capitalize(),
+        })
+    else:
+        return render(request, "encyclopedia/entry.html", {
+            "entry": f"Error: requested page {title.capitalize()} was not found",
+            "page_title": title.capitalize(),
+        })
