133386d22681e913263480786564a9cc835fb053
Ziga12341 <Ziga12341@users.noreply.github.com>
2023-02-22 21:40:06 +0100

    automated commit by submit50

--- wiki/encyclopedia/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import show_entry, index
-
-app_name = "wiki"
-urlpatterns = [
-    path("", index, name="index"),
-    path("<str:title>", show_entry, name="entries"),
-]
